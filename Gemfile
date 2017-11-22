056dbd8f7dc81b50d47bdd003a62e8ffcf588e96
thomas quelin <th.quelin@gmail.com>
2017-11-22 16:54:58 +0100

    1er exercice terminé

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'bootstrap-sass'
 gem 'font-awesome-sass'
 gem 'simple_form'
 gem 'autoprefixer-rails'
+gem 'jquery-rails'
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+
+
+puts "Creating restaurants"
+kano = Restaurant.create( {
+  name: "Chez Kano",
+  address: "24, rue Marbeuf",
+  phone_number: "0145628789",
+  category: "japanese"
+  } )
+
+georges = Restaurant.create( {
+  name: "Chez Georges",
+  address: "22, rue Cortambert",
+  phone_number: "0156734564",
+  category: "french",
+  } )
+
+luigi = Restaurant.create( {
+  name: "Chez Luigi",
+  address: "31, rue du faubourg Poissonière",
+  phone_number: "0167654532",
+  category: "italian",
+  } )
+
+christophe = Restaurant.create( {
+  name: "Chez Christophe",
+  address: "46, rue du Mont-Cenis",
+  phone_number: "0145623676",
+  category: "belgian"
+  } )
+
+yiang = Restaurant.create( {
+  name: "Chez Yiang",
+  address: "76, rue Montmartre",
+  phone_number: "0145657865",
+  category: "chinese"
+  } )
